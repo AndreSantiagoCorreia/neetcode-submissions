c52a73bff398ed9957bbccc115862aafac4ea851
André Santiago Correia <65636726+AndreSantiagoCorreia@users.noreply.github.com>
2026-05-09 17:42:31 -0400

    Add: find-target-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-3.py
@@ -0,0 +1,54 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        """
+        3   4   5   6   1   2
+        L       m           R
+        nums[m] > nums[r]
+        l = m + 1
+                    l   m   r       
+        nums[m] < nums[r]
+        r = m
+                    l   r
+        nums[m] < nums[r]
+        r = m (now r == l)
+
+        - Now we have two sorted arrays
+        --1: [0, l-1]
+        --2: [l, r]
+        First, evaluate which array we should proceed with Binary Search
+        - Is target within (1) or (2)?
+        Perform Binary Search on previous answered array
+        """
+        if not nums:
+            return -1
+            
+        # PASS 1: Find the index of the pivot (the minimum element)
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = l + (r - l) // 2
+            if nums[m] > nums[r]:
+                l = m + 1  # Pivot is in the right half
+            else:
+                r = m      # Pivot is at m or in the left half
+                
+        pivot = l
+        
+        # PASS 2: Determine which sorted subarray contains the target
+        l, r = 0, len(nums) - 1
+        if target >= nums[pivot] and target <= nums[r]:
+            l = pivot      # Search in the right sorted subarray
+        else:
+            r = pivot - 1  # Search in the left sorted subarray
+            
+        # Standard Binary Search
+        while l <= r:
+            m = l + (r - l) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+                
+        return -1
+        
